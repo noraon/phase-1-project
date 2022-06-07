f9bced1c780952d947dd96dc288dc6d9b4fcd4ec
Nora <noraonyi@gmail.com>
2022-06-07 17:50:55 -0400

    Completed assignment

--- code/phase-1-api-project/main.js
+++ code/phase-1-api-project/main.js
@@ -1,16 +1,45 @@
-document.addEventListener( 'DomContentLoaded',() =>{
-    console.log(document.querySelector(div))
+document.addEventListener( 'DOMContentLoaded',()=> {
+
+
+let form =document.querySelector('activityForm')
+const activityForm = document.getElementById('activityForm')
+console.log(activityForm)
+activityForm.addEventListener('submit', (e) =>{
+ e.preventDefault()
+ document.querySelector('#activityForm')
+
+// })
+ 
+// form.reset();
+    // themeBtn.addEventListener=(click,(e)=> {
+    // document.querySelector('body')
+    // })
+    console.log(e.target.value)
+    console.log('I am clicked')
 })
-console.log(document.querySelector('div'))
+
+let card = document.createElement(li)
+card.className='card'
+card.innerHTML=
+"<div class='content'></div>"
+
+fetchData(searchActivity.value)
+entersearchActivity=enterSearchactivity;
+if(entersearchActivity){
+    searchFormContainer.style.display = "block";
+ } else {
+        searchFormContainer.style.display="none";
+    }
 
 const apiData =() => {
-    return fetch('//www.boredapi.com/api/activity')
-    
+    return fetch('http://www.boredapi.com/api/activity')
 
-   .then (response =>response.json())
+    
+    
+    .then (response =>response.json())
     .then (json => {
 
-        renderApiData(json)
+        console.log(json)
     }).catch(error =>{
         console.log(error)
     
@@ -20,25 +49,40 @@ apiData()
 
 
 const renderApiData =(json => {
-    const top10Activities = document.querySelector('#top10Activities')
+    const top10Activities = document.querySelector('#activity-list')
     
 
     json.forEach(activity=> {
         
     })
     
+}
+
+//e.prevent default prevents the page from refreshing when a form is submitted
+//get the form input from e.target
+
+)
+
+let p=document.createElement('select')
+document.querySelector('#activity-list').appendChild(card)
+
+console.log(e.target.querySelector('#mysearch'.value))
+activitySearch(e.target.querySelector)('mySearch').value
+activitySearch.reset
+
+p.textContent = '$(todo)';
+priority.innerHTML =
+'<option>Priority</option>'
+'<option>Medium</option>'
+'<option>Low</option>'
+
+edbtn.textContent = 'Edit';
+edbtn.textContent = 'Completed';
+
+edbtn.addEventListener('click,handleEdit');
+edbtn.addEventListener('click','handleDelete')
+
+
+p.appendChild(activity-list)
+
 })
-
-
-const form = document.getElementById("select-activities")
-console.log(form) 
-form.addEventListener ("submit",(e) => {
-    e.preventDefault()
-    console.log(e.target)
-
-
-})
-
-
-
-
---
+++ code/phase-1-api-project/readme.md
@@ -0,0 +1,3 @@
+As a reader,I should be able to search for activities using a dropdown menu.
+As a user, I should be able submit my result using the submit form.
+As a user, I expect to see task string appear in the DOM after submit button has been activated.
